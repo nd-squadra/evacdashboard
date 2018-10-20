83700109aa839397c221426545d30e6444d1090c
IvanFengJK <ivanfengjunkai1997114@gmail.com>
2018-10-21 00:38:38 +0800

    spelling mistake

--- src/alerters.js
+++ src/alerters.js
@@ -16,7 +16,7 @@ class Alerters extends React.Component{
                 <div className="valign">
                   <h1 className="alertsDashboard">
                   <img className="alertsSymbols" src={require("./question.png")}></img>
-                  Suspicious of domestric abuse?
+                  Suspicious of domestic abuse?
                   <button type="submit" value="Review" className="Review" > Check </button>
                   </h1>
                 </div>
